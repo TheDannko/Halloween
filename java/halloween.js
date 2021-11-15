2d63c90f9ee59b645620f3f38aca858ae7a6118a
TheDannko <92975314+TheDannko@users.noreply.github.com>
2021-11-15 20:26:23 +0100

    Add files via upload

---
+++ java/halloween.js
@@ -0,0 +1,17 @@
+/******* Javascripts de la presentación de halloween de Daniel ********/
+
+function mostrar(imagen){
+    document.getElementById(imagen).style.visibility="visible";
+}
+
+function ocultar(imagen){
+    document.getElementById(imagen).style.visibility="hidden";
+}
+
+function sonar(sonido){
+    document.getElementById(sonido).play();
+}
+
+function pausar(sonido){
+    document.getElementById(sonido).pause()
+}
